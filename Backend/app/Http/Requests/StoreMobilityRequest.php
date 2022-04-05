5d643bad47646d8e9b05ddf9abe9c88b4c1bc7d7
kristytoman <k_tomanova@utb.cz>
2022-04-05 11:01:05 +0200

    [Backend] Add mobility create endpoint

---
+++ Backend/app/Http/Requests/StoreMobilityRequest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class StoreMobilityRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'university_id' => 'required|exists:universities,id',
+            'arrival' => 'required|date|before:departure|before:today',
+            'departure' => 'required|date|after:arrival|before:today',
+            'student_id' => 'required|required|exists:students,id',
+        ];
+    }
+
+
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success'   => false,
+            'code' => 422,
+            'message'   => 'Validation errors',
+            'data'      => $validator->errors()
+        ]));
+    }
+
+    /**
+     * Handle a failed authorization attempt.
+     *
+     * @return void
+     *
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    protected function failedAuthorization()
+    {
+        throw new HttpResponseException(response()->json([
+            'success'   => false,
+            'code' => 403,
+            'message'   => 'Unauthorized access',
+            'data'      => 'The request is not authorized.'
+        ]));
+    }
+}
